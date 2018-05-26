6cdd98251c3fb47889e1666d192404d0c5120fe5
KINOKI <raphael@lesitevideo.net>
2018-05-26 10:11:16 +0200

    Add files via upload

---
+++ stitch_debian.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+SECONDS=0;
+list=(./enfused/*)
+
+echo $((${#list[@]})) fichiers
+echo $((${#list[@]}/4)) panos
+
+nd=$((${#list[@]}/4))
+
+nf=4                     
+
+
+for (( i=1; i<=nd; i++ )); do
+	filestostitch=${list[@]:(i-1)*nf:nf}
+	stitchto="./panos/pano"$i".jpg"
+	
+	echo $filestostitch
+	echo $stitchto
+
+		FN="stitched.pto"
+		OFN="stitched"
+		FOV=180  #118  #109.7
+		projectionNumber=2 #0 - rectilinear, 2 - equirectangular, 3 - full-frame fisheye ...
+		maskName="boardInTheMiddle.msk"
+		
+		yaw0=90
+		yaw1=0
+		yaw2=180
+		yaw3=270
+		pitch0=0
+		pitch1=0
+		pitch2=0
+		pitch3=0
+		roll0=0
+		roll1=0
+		roll2=0
+		roll3=0
+		
+		pto_gen -o $FN -f $FOV -p $projectionNumber $filestostitch
+        
+		# get control points
+		cpfind -v --multirow --celeste -o $FN $FN
+        
+        #control errors cpfind
+        ret=$?
+        if [ $ret -ne 0 ]
+        then
+          echo "ERROR cpfind: $ret"
+          exit $ret
+        fi
+        
+		celeste_standalone -d ./celeste.model -i $FN -o $FN
+
+		cpclean -o $FN $FN
+        linefind -o $FN $FN
+		autooptimiser -a -l -s -o $FN $FN	
+		
+		# panorama options (straighten, set FOV to auto, optimal size)
+		pano_modify -s -c --canvas=AUTO -o $FN $FN
+		
+		for r in $(seq 0 3)
+		do
+			nona -z LZW -r ldr -m TIFF_m -o $OFN -i $r $FN
+			#nona -r ldr -m PNG_m -o $OFN -i $r $FN
+		done
+		
+		enblend --compression=LZW -w -f17058x4131+0+2422 -o "${OFN}.tif" -- "${OFN}0000.tif" "${OFN}0001.tif" "${OFN}0002.tif" "${OFN}0003.tif"
+		#enblend --compression=jpeg -o stitched.jpg -- stitched0000.png stitched0001.png stitched0002.png stitched0003.png
+		#enblend -o stitched.png -- stitched0000.png stitched0001.png stitched0002.png stitched0003.png
+
+
+		imgwidth=$(identify -format '%w' stitched.tif)
+		imgheight=$(identify -format '%h' stitched.tif)
+		
+		echo "Burning final jpeg (normalized), size :"
+		echo $((imgwidth))x$(( imgwidth/2 ))
+
+
+		convert stitched.tif -background black -sharpen 0x1.0 -normalize -extent $((imgwidth))x$(( imgwidth/2 )) $stitchto
+		
+		echo "Exifing panorama ..."
+		exiftool -overwrite_original -Make=Kinoki.fr -Model=KINOBOT -Software=KINOSTITCH -FullPanoWidthPixels=$((imgwidth)) -FullPanoHeightPixels=$(( imgwidth/2 )) -CroppedAreaImageWidthPixels=$((imgwidth)) -CroppedAreaImageHeightPixels=$(( imgwidth/2 )) -CroppedAreaLeftPixels=0 -CroppedAreaTopPixels=0 -UsePanoramaViewer=True -ProjectionType=equirectangular -PoseHeadingDegrees=0 $stitchto
+		
+		rm "${OFN}0000.tif" "${OFN}0001.tif" "${OFN}0002.tif" "${OFN}0003.tif" "${FN}" stitched.tif
+
+		echo "fini "$stitchto
+	
+done
+
+echo "fini de stitcher en "$SECONDS" secondes"
